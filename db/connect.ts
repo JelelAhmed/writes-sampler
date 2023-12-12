0cf38873f2092dc0ffa428691b4251b94699308a
jelelahmed <abduljelelahmed>
2023-12-12 09:58:13 +0100

    implemented callback function to handle CRUD operations on the mongo database

--- db/connect.ts
+++ db/connect.ts
@@ -13,22 +13,23 @@ import { Db, MongoClient } from 'mongodb'
 global.mongo = global.mongo || {}
 
 export const connectToDB = async () => {
-	if (!global.mongo) {
-		const client = new MongoClient(process.env.Database_URL, {
-			useUnifiedTopology: true,
-			useNewUrlParser: true,
-			bufferMaxEntries: 0,
-			connectTimeoutMS: 10000
-		})
+	if (!global.mongo.client) {
+    global.mongo.client = new MongoClient(process.env.DATABASE_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      bufferMaxEntries: 0,
+      connectTimeoutMS: 10000,
+    })
 
-		console.log('connecting to DB')
-		await global.mongo.client.connect()
-		console.log('connected to DB')
+    console.log('connecting to DB')
+    await global.mongo.client.connect()
+    console.log('connected to DB')
+  }
 
-		global.mongo.client = client
-	}
-
-  const db = global.mongo.client.db('sampler')
+  const db: Db = global.mongo.client.db('sampler')
 
   return { db, dbClient: global.mongo.client }
 }
+
+
+
--- pages/app/[[...id]].tsx
+++ pages/app/[[...id]].tsx
@@ -1,6 +1,7 @@
 import React, { FC, useState } from 'react'
 import { Pane, Dialog, majorScale } from 'evergreen-ui'
 import { useRouter } from 'next/router'
+import { getSession, useSession } from 'next-auth/client'
 import Logo from '../../components/logo'
 import FolderList from '../../components/folderList'
 import NewFolderButton from '../../components/newFolderButton'
@@ -8,7 +9,9 @@ import User from '../../components/user'
 import FolderPane from '../../components/folderPane'
 import DocPane from '../../components/docPane'
 import NewFolderDialog from '../../components/newFolderDialog'
-import { getSession, useSession } from 'next-auth/client'
+import { folder, doc, connectToDB } from '../../db'
+
+
 
 const App: FC<{ folders?: any[]; activeFolder?: any; activeDoc?: any; activeDocs?: any[] }> = ({
   folders,
@@ -63,7 +66,7 @@ const App: FC<{ folders?: any[]; activeFolder?: any; activeDoc?: any; activeDocs
           <NewFolderButton onClick={() => setIsShown(true)} />
         </Pane>
         <Pane>
-          <FolderList folders={[{_id: 1, name: 'hello'}]} />{' '}
+          <FolderList folders={folders} />{' '}
         </Pane>
       </Pane>
       <Pane marginLeft={300} width="calc(100vw - 300px)" height="100vh" overflowY="auto" position="relative">
@@ -79,12 +82,34 @@ App.defaultProps = {
   folders: [],
 }
 
-export async function getServerSideProps(ctx) {
-	const session = await getSession(ctx)
+export async function getServerSideProps(context) {
+  const session = await getSession(context)
+  // not signed in
+  if (!session || !session.user) {
+    return { props: {} }
+  }
 
-	return {
-		props: { session },
-	}
+  const props: any = { session }
+  const { db } = await connectToDB()
+  const folders = await folder.getFolders(db, session.user.id)
+  props.folders = folders
+
+  if (context.params.id) {
+    const activeFolder = folders.find((f) => f._id === context.params.id[0])
+    const activeDocs = await doc.getDocsByFolder(db, activeFolder._id)
+    props.activeFolder = activeFolder
+    props.activeDocs = activeDocs
+
+    const activeDocId = context.params.id[1]
+
+    if (activeDocId) {
+      props.activeDoc = await doc.getOneDoc(db, activeDocId)
+    }
+  }
+
+  return {
+    props,
+  }
 }
 
 
